80e115074a7cee9edf90534be857b4a784167bf9
inna4ka <innanochevkina@gmail.com>
2020-04-17 17:13:49 +0300

    commit
    web

---
+++ mobjs.js
@@ -0,0 +1,39 @@
+function mobf()
+{
+	var el=document.getElementById("mob");
+	var el1=document.getElementById("mob2");
+	var el2=document.getElementById("mob3");
+	if(el.style.display!='block')
+	{
+		el.style.display ='block'
+		el1.style.display ='none';
+		el2.style.display ='none';
+	}
+	else{el.style.display ='none'}
+}
+function mobf2()
+{
+	var el=document.getElementById("mob");
+	var el1=document.getElementById("mob2");
+	var el2=document.getElementById("mob3");
+	if(el.style.display!='block')
+	{
+		el1.style.display ='block'
+		el.style.display ='none';
+		el2.style.display ='none';
+	}
+	else{el.style.display ='none'}
+}
+function mobf3()
+{
+	var el=document.getElementById("mob");
+	var el1=document.getElementById("mob2");
+	var el2=document.getElementById("mob3");
+	if(el.style.display!='block')
+	{
+		el2.style.display ='block'
+		el.style.display ='none';
+		el1.style.display ='none';
+	}
+	else{el.style.display ='none'}
+}
